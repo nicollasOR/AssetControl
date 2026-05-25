e181d9e60f70bc06af371b1772f87a25b2fab71c
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-05-24 21:12:17 -0300

    🎉  feat: started frontend 🎉

---
+++ asset-control/src/pages/api/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import secureLocalStorage from 'react-secure-storage'
+
+const apiLocal = "https://localhost:7104/api/"
+
+export const api = axios.create({ 
+    baseURL: apiLocal
+})
+
+api.interceptors.request.use((config => {
+    const token = secureLocalStorage.getItem("Token")
+    if(token)
+        config.headers.Authorization = "Bearer" + token
+    return config
+}))
